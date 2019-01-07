b60536baa272fb630db77cb23c5480dc4eb0f319
Bali <lisiyuan@tengyue360.com>
2019-01-07 17:31:25 +0800

    V1.0

---
+++ build/precache-manifest.25e46028d8d7756abe9d7f1369ff1cab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "47bb672d6240b975228842750bdb5932",
+    "url": "./static/media/ele-logo.47bb672d.svg"
+  },
+  {
+    "revision": "8b18ce43110c94a167c8",
+    "url": "./static/css/main.a79015d7.chunk.css"
+  },
+  {
+    "revision": "b1c139d7e9e48c6859a1ef8827eebca5",
+    "url": "./static/media/iconfont.b1c139d7.ttf"
+  },
+  {
+    "revision": "c779f8c1da41cc61b7ab",
+    "url": "./static/js/1.c779f8c1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6954e43f78da12f6c0db2dcbda140ed5",
+    "url": "./static/media/bg.6954e43f.jpg"
+  },
+  {
+    "revision": "c999832b5f5065a02e3ad1733801f77b",
+    "url": "./static/media/github-logo.c999832b.svg"
+  },
+  {
+    "revision": "8b18ce43110c94a167c8",
+    "url": "./static/js/main.8b18ce43.chunk.js"
+  },
+  {
+    "revision": "1e58fcaaf6f6334a2a8e0937e98cf92c",
+    "url": "./static/media/mobx-logo.1e58fcaa.png"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "./static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "./static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d19046b18c814726d2eac0684feeabb4",
+    "url": "./static/media/iconfont.d19046b1.eot"
+  },
+  {
+    "revision": "c779f8c1da41cc61b7ab",
+    "url": "./static/css/1.135ba3d2.chunk.css"
+  },
+  {
+    "revision": "34a99c228c0bdd49a1ab10d2113b7616",
+    "url": "./index.html"
+  }
+];
