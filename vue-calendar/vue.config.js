114bbca1f13f25a8addcd6a169bcf98db4531499
Bali <lisiyuan@tengyue360.com>
2019-03-22 11:36:34 +0800

    vue-super-calendar

---
+++ vue-calendar/vue.config.js
@@ -0,0 +1,11 @@
+// const pac = require('./package.json')
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// const htmlOptions = {
+//   template: 'public/index.html',
+//   filename: process.env.NODE_ENV === 'development' ? 'index.html' : str + '.html',
+//   inject: true
+// }
+module.exports = {
+    publicPath: '././'
+}
